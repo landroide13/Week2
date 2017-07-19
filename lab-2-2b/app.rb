dbb6288b28efa36639ddccb3bee791ef0c6101a5
Luis Andrade reuss <landrade@MacBook-Pro-de-Luis.local>
2017-07-19 14:28:49 +0700

    layout

--- lab-2-2b/app.rb
+++ lab-2-2b/app.rb
@@ -3,14 +3,14 @@ require 'erb'
 
 
 get "/" do
-  name="Hello"
-  erb :"index.html" locals:{name:name}
+  
+  erb :"index.html" ,locals:{name:name},layout: :"layout.html"
 end
 
 
 get "/about" do
    name = "Your name here"
-  erb :"about.html" , locals:{name:name}
+  erb :"about.html" , locals:{name:name} , layout: :"layout.html"
 end
 
 
