870e4e0e887ce97ff2c53770bb12824df7a9e437
Luis Andrade reuss <landrade@MacBook-Pro-de-Luis.local>
2017-07-19 11:51:51 +0700

    wensday

--- lab-2-3a.rb
+++ lab-2-3a.rb
@@ -3,10 +3,17 @@ require 'erb'
 
 item = [{name: "Attend lectures", done: true},
   {name: "Do labs", done: true},
-  {name: "Build stuff", done: false}]
+  {name: "Build stuff", done: false}
+]
 
-template = <% if item[:done?] %>
-           <% item[:name] %>
+template = "<%  item.each do |item| %>
+               <% if item[:done]  %> 
+                [x] <%= item[:name] %>
+             <% else %>
+                [ ] <%= item[:name] %>
+             <% end %>
+            <% end %>"
+
 
 puts ERB.new(template).result(binding)
 
